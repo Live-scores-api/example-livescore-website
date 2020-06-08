993cbc2cdaf2874601ce6b9e14441489c43478c3
Hristo Petev <hgpetev@gmail.com>
2020-06-08 12:49:01 +0300

    Formatter the code

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-
 include 'config.php';
 include 'functions.php';
 include 'classes/LivescoreApi.class.php';
@@ -7,7 +6,6 @@ include 'classes/LivescoreApi.class.php';
 $Api = new LivescoreApi();
 $data = $Api->getLivescores();
 $timezone = 'Europe/Istanbul';
-
 ?>
 
 <html>
@@ -16,32 +14,32 @@ $timezone = 'Europe/Istanbul';
 	</head>
 	<body>
 		<div class="container">
-      <div class="row">
-        <div class="col-md-3">
-        </div>
-        <div class="col-md-6">
-          <table class="table table-bordered">
-            <tr class="table-info">
-              <th>KO</th>
-              <th>Time</th>
-              <th>Home</th>
-              <th>Score</th>
-              <th>Away</th>
-            </tr>
-            <?php foreach ($data['data']['match'] as $_match) { ?>
-              <tr>
-                <td><?=convert($_match['scheduled'], $timezone)?></td>
-                <td><?=$_match['time']?></td>
-                <td style="text-align: right;"><?=$_match['home_name']?></td>
-                <td style="text-align: center;"><?=$_match['score']?></td>
-                <td><?=$_match['away_name']?></td>
-              </tr>
-            <?php } ?>
-          </table>
-        </div>
-        <div class="col-md-3">
-        </div>
-      </div>
-    </div>
+			<div class="row">
+				<div class="col-md-3">
+				</div>
+				<div class="col-md-6">
+					<table class="table table-bordered">
+						<tr class="table-info">
+							<th>KO</th>
+							<th>Time</th>
+							<th>Home</th>
+							<th>Score</th>
+							<th>Away</th>
+						</tr>
+							<?php foreach ($data['data']['match'] as $_match) { ?>
+							<tr>
+								<td><?= convert($_match['scheduled'], $timezone) ?></td>
+								<td><?= $_match['time'] ?></td>
+								<td style="text-align: right;"><?= $_match['home_name'] ?></td>
+								<td style="text-align: center;"><?= $_match['score'] ?></td>
+								<td><?= $_match['away_name'] ?></td>
+							</tr>
+							<?php } ?>
+					</table>
+				</div>
+				<div class="col-md-3">
+				</div>
+			</div>
+		</div>
 	</body>
 </html>
